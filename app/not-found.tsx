9cdf06fdda05f659458e1f84758e22af50f4d212
Alexander Denisov <68102323+pr0gra@users.noreply.github.com>
2024-07-24 19:40:15 +0500

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div style={{ height: "50vh", display: "flex", justifyContent: "center", alignItems: "center", color: "white", fontFamily:"strong" }}>
+            Страница не найдена...
+        </div>
+    );
+}
